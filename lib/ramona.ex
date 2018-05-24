9b255593050af2eab4b654636ee5d12faa09620c
appositum <eddie.appositum@gmail.com>
2018-05-24 01:42:48 -0300

    Morse encoder/decoder

--- lib/ramona.ex
+++ lib/ramona.ex
@@ -23,6 +23,7 @@ defmodule Ramona do
   defp load_modules do
     use Ramona.Events
     use Ramona.Commands.Basic
+    use Ramona.Commands.Morse
     use Ramona.Commands.Random
     use Ramona.Commands.Moderation
   end
