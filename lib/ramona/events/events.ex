2bcfbb274c161238e31881ba64fa9639cf2b7fb6
appositum <appositum@protonmail.com>
2018-08-04 05:17:30 -0300

    Block invites

--- lib/ramona/events/events.ex
+++ lib/ramona/events/events.ex
@@ -1,11 +1,17 @@
 defmodule Ramona.Events do
   @moduledoc false
   use Alchemy.Events
-  alias Alchemy.{Cache, Cogs}
+  alias Alchemy.{Cache, Client}
   require Logger
+  require Alchemy.Cogs, as: Cogs
+
+  @ansuz "429110513117429780"
+  @eihwaz "429111918297612298"
+  @moderation_cat "430410176328368150"
 
   Events.on_ready(:ready)
   Events.on_message(:command_log)
+  Events.on_message(:block_invites)
 
   def ready(_, _) do
     me = "#{Cache.user().username}##{Cache.user().discriminator} (#{Cache.user().id})"
@@ -31,5 +37,45 @@ defmodule Ramona.Events do
         |> Logger.info()
       end
     end
+  end
+
+  def block_invites(message) do
+    {:ok, channel} = Client.get_channel(message.channel_id)
+
+    unless message.author.id == Cache.user.id
+    or channel.parent_id == @moderation_cat
+    do
+      {:ok, guild_id} = Cogs.guild_id()
+
+      case Client.get_member(guild_id, message.author.id) do
+        {:error, reason} ->
+          "Tried to fetch user (possibly a bot) "
+          <> "<@#{message.author.id}>, but failed:\n#{reason}"
+          |> Logger.error()
+
+        {:ok, member} ->
+          IO.inspect no_permission(member), label: "CU"
+          if no_permission(member)
+          and Regex.run(~r{discord\.gg\/[a-zA-Z0-9]*}, message.content) do
+            with {:ok, channel} <- Alchemy.Client.get_channel(message.channel_id),
+                 {:ok, nil} <- Alchemy.Client.delete_message(message)
+            do
+              "Blocked invite in ##{channel.name} (#{channel.id}) from "
+              <> "#{message.author.username}##{message.author.discriminator} "
+              <> "(#{message.author.id})"
+              |> Logger.warn()
+              IO.inspect(message)
+            else
+              err ->
+                IO.inspect err
+                Logger.error(err)
+            end
+          end
+      end
+    end
+  end
+
+  defp no_permission(member) do
+    @ansuz not in member.roles and @eihwaz not in member.roles
   end
 end
