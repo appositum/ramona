281fb946a37b1621d3c768cc38680c88a8476084
appositum <appositum@pm.me>
2023-11-06 20:05:32 -0300

    Update nullable type signature

--- lib/ramona/utils.ex
+++ lib/ramona/utils.ex
@@ -139,7 +139,7 @@ defmodule Ramona.Utils do
     String.replace(message, prefix, "\\#{prefix}")
   end
 
-  @spec invite_match?(String.t()) :: boolean
+  @spec invite_match?(String.t()) :: nil | boolean
   def invite_match?(str) do
     Regex.run(~r{discord\.gg\/[a-zA-Z0-9]*}, str) ||
     Regex.run(~r{discordapp\.com\/invite\/[a-zA-Z0-9]*}, str)
