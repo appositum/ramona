fbfd88305d07a91f4204d51b8403a46888492a33
appositum <appositum@pm.me>
2023-11-06 20:00:17 -0300

    Add files

---
+++ lib/ramona.ex
@@ -0,0 +1,28 @@
+defmodule Ramona do
+  @moduledoc false
+  use Application
+  alias Alchemy.{Client, Cogs}
+
+  def start(_type, _args) do
+    case Application.get_env(:ramona, :token) do
+      nil ->
+        raise "TOKEN environment variable is not set"
+      token ->
+        prefix = Application.fetch_env!(:ramona, :prefix)
+        bootstrap(token, prefix)
+    end
+  end
+
+  defp bootstrap(token, prefix) do
+    run = Client.start(token)
+    load_modules()
+    Cogs.set_prefix(prefix)
+    run
+  end
+
+  defp load_modules do
+    use Ramona.Events
+    use Ramona.Commands.Basic
+    use Ramona.Commands.Moderation
+  end
+end
---
+++ lib/ramona/events.ex
@@ -0,0 +1,34 @@
+defmodule Ramona.Events do
+  @moduledoc false
+  use Alchemy.Events
+  alias Alchemy.{Cache, Client, Cogs}
+  require Logger
+
+  Events.on_ready(:ready)
+  def ready(_, _) do
+    me = "#{Cache.user.username}##{Cache.user.discriminator} (#{Cache.user.id})"
+    Logger.info("Logged in as #{me}")
+    Logger.info("Ramona is ready!")
+  end
+
+  Events.on_message(:command_log)
+  def command_log(message) do
+    prefix = Application.fetch_env!(:ramona, :prefix)
+
+    if String.starts_with?(message.content, prefix) do
+      command =
+        with m <- message.content |> String.split() |> List.first() do
+          String.slice(m, 2, String.length(m))
+        end
+
+      if command in Map.keys(Cogs.all_commands()) do
+        id = message.author.id
+        username = message.author.username
+        tag = message.author.discriminator
+
+        ~s/Command "#{message.content}" called by <@#{id}> (#{username}##{tag})/
+        |> Logger.info()
+      end
+    end
+  end
+end
